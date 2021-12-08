aca60fa79dcfbf402ab92223726e5081231e2465
mdisoft126 <mdisoft126@gmail.com>
2021-12-08 21:39:34 +0100

    bean property, constructor injection and autowired annotation

--- src/main/java/com/marcind/demospring/App.java
+++ src/main/java/com/marcind/demospring/App.java
@@ -12,7 +12,11 @@ public class App
     public static void main( String[] args )
     {
     	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Vehicle obj = (Vehicle)context.getBean("bike");
+        
+    	Car obj = (Car)context.getBean("car");
         obj.drive();
+    	
+//    	Tyre t = (Tyre) context.getBean("tyre");
+//    	System.out.println(t);
     }
 }
