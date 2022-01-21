0755dad39f8d498c81ea8c0ccdeb3ac65e3a5d69
mdisoft126 <mdisoft126@gmail.com>
2022-01-21 23:22:18 +0100

    Annotation RequestParam added

--- src/main/java/com/marcind/AddController.java
+++ src/main/java/com/marcind/AddController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.marcind.service.AddService;
@@ -13,10 +14,10 @@ import com.marcind.service.AddService;
 public class AddController 
 {
 	@RequestMapping("/add")
-	public ModelAndView add(HttpServletRequest request, HttpServletResponse response)
+	public ModelAndView add(@RequestParam("t1") int i, @RequestParam("t2") int j, HttpServletRequest request, HttpServletResponse response)
 	{
-		int i = Integer.parseInt(request.getParameter("t1"));
-		int j = Integer.parseInt(request.getParameter("t2"));
+//		int i = Integer.parseInt(request.getParameter("t1"));
+//		int j = Integer.parseInt(request.getParameter("t2"));
 //		int k = i + j;
 		
 		AddService as = new AddService();
