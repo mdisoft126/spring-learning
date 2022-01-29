f7bf62cc32cfd8d1e1fa3b7104eb0fd1be91a51b
mdisoft126 <mdisoft126@gmail.com>
2022-01-30 00:11:11 +0100

    WebApp completed

--- myWebApp/src/main/java/com/marcind/demo/HomeController.java
+++ myWebApp/src/main/java/com/marcind/demo/HomeController.java
@@ -5,19 +5,21 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController 
 {
 	
 	@RequestMapping("home")
-	public String home(HttpServletRequest req)
+	public ModelAndView home(Alien alien)
 	{
-		HttpSession session = req.getSession();
-		String name = req.getParameter("name");
-		System.out.println("hi " + name);
-		session.setAttribute("name", name);
-		return "home";
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("obj", alien);
+		mv.setViewName("home");
+		
+		return mv;
 		
 	}
 }
--- myWebApp/src/main/webapp/pages/home.jsp
+++ myWebApp/src/main/webapp/pages/home.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	Welcome ${name}
+	Welcome ${obj.aid}, ${obj.aname}, ${obj.lang}
 </body>
 </html>
